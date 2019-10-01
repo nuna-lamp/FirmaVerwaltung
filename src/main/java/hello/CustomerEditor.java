626c6f3f0843a83700e806ab8f99c793bab8d070
Nuna <nuna@lamp-solutions.de>
2019-10-01 12:01:09 +0200

    comment remove

--- src/main/java/hello/CustomerEditor.java
+++ src/main/java/hello/CustomerEditor.java
@@ -12,23 +12,12 @@ import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
 import org.springframework.beans.factory.annotation.Autowired;
 
-/**
- * A simple example to introduce building forms. As your real application is probably much
- * more complicated than this example, you could re-use this form in multiple places. This
- * example component is only used in MainView.
- * <p>
- * In a real world application you'll most likely using a common super class for all your
- * forms - less code, better UX.
- */
 @SpringComponent
 @UIScope
 public class CustomerEditor extends VerticalLayout implements KeyNotifier {
 
 	private final CustomerRepository repository;
 
-	/**
-	 * The currently edited customer
-	 */
 	private Customer customer;
 
 	/* Fields to edit properties in Customer entity */
